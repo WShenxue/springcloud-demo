217174319f35cf49e37f4e808705b97720f1f99e
WShenxue <wanshenxue@163.com>
2018-08-05 23:48:19 +0800

    mock Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
-import { FormsModule } from '@angular/forms';
+import { HEROES } from './../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +14,20 @@ export class HeroesComponent implements OnInit {
     name: "Widstorm"
   }
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  //public onSelect(hero) {
+  //  this.selectedHero = hero;
+  //}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
