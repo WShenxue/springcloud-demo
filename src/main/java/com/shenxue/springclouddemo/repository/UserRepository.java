f74529fefd66c544d2d08e34130f5797d3d5698e
WShenxue <wanshenxue@163.com>
2018-09-10 23:01:40 +0800

    del

--- src/main/java/com/shenxue/springclouddemo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shenxue.springclouddemo.repository;
-
-import com.shenxue.springclouddemo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
