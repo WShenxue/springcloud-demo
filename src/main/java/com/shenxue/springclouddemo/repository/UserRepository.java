facea638cf6c392d9f206417036086900c8739ac
WShenxue <wanshenxue@163.com>
2018-09-10 22:38:31 +0800

    started demo

---
+++ src/main/java/com/shenxue/springclouddemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shenxue.springclouddemo.repository;
+
+import com.shenxue.springclouddemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
